b3b73934c9a14fa7274dd1a0e42bf1a7e0ec452d
Guy Stitt <guyjstitt@gmail.com>
2016-10-14 11:55:20 -0400

    fixing paths for gulp

--- gulpfile.js
+++ gulpfile.js
@@ -13,12 +13,12 @@ var elixir = require('laravel-elixir');
 
 elixir(function(mix) {
     var bootstrapPath = 'node_modules/bootstrap-sass/assets';
-    var masonryPath = 'node_modules/masonry-layout/dist/';
-    var jqueryPath = 'node_modules/jquery/dist/';
-    var imagesLoadedPath = 'node_modules/imagesLoaded/imagesloaded.pkgd.min.js';
+    var masonryPath = 'node_modules/masonry-layout/dist';
+    var jqueryPath = 'node_modules/jquery/dist';
+    var imagesLoadedPath = 'node_modules/imagesloaded/imagesloaded.pkgd.min.js';
     mix.sass('app.scss')
         .copy(bootstrapPath + '/fonts', 'public/fonts')
-        .copy(jqueryPath + 'jquery.min.js', 'public/js')
+        .copy(jqueryPath + '/jquery.min.js', 'public/js')
         .copy(bootstrapPath + '/javascripts/bootstrap.min.js', 'public/js')
         .copy(masonryPath + '/masonry.pkgd.min.js', 'public/js')
         .copy(imagesLoadedPath, 'public/js');
