bda4922a33dc80c410095fba966c368f00c9e4ae
Guy Stitt <guyjstitt@gmail.com>
2017-06-15 22:09:55 -0400

    adding amazon link

--- resources/views/books-and-blogs.blade.php
+++ resources/views/books-and-blogs.blade.php
@@ -9,7 +9,7 @@
 
           <div class="col-md-4 col-sm-6 item">
             <div class="thumbnail">
-            <a class="imageLink" target="blank" href="https://l.facebook.com/l.php?u=https%3A%2F%2Fwww.amazon.com%2FNero-Demare-Shadows-Legend-Vampires-ebook%2Fdp%2FB01LVTMLR0&h=ATOg7RB962RVnTY8joNOR3WYkjcucbSTVQcmIOXBn94MATs1JxdQ_CzvtrwQplhOVaUrjfY3N_GByJgBJNWCID-FiUBJwGc0cUSQ0UdMoqKCi7jN7CSDR3r8kw_6CigEfgn-WLi8lgh0RtcHvcf5o28htvcExPzy"></a>
+            <a class="imageLink" target="blank" href="https://www.amazon.com/Nero-Demare-Shadows-Legend-Vampires-ebook/dp/B01LVTMLR0"></a>
               <img src="/images/nero-3d-500.png" alt="">
             </div>
           </div><!--/.item  -->
